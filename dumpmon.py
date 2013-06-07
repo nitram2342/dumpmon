ae8df76230d87fdab78eb90fafd32e5ef5bf0c57
Ciccio87 <ciccio87@f3sa>
2013-06-07 03:49:30 +0200

    added nopaste.me

--- dumpmon.py
+++ dumpmon.py
@@ -53,8 +53,10 @@ def monitor():
         target=Pastie().monitor, args=[bot, tweet_lock])
     pastebin_ru_thread = threading.Thread(
     	target=Pastebin_ru().monitor, args=[bot, tweet_lock])
+    nopaste_thread = threading.Thread(
+		target=Nopaste().monitor, args = [bot, tweet_lock])
 
-    for thread in (pastebin_thread, slexy_thread, pastie_thead, pastebin_ru_thread):
+    for thread in (pastebin_thread, slexy_thread, pastie_thead, pastebin_ru_thread, nopaste_thread):
         thread.daemon = True
         thread.start()
 
